2f82a201fe9172fc89ef78406bae064a0fd045d4
Sumanta Ghosh <sg4452095@gmail.com>
2024-12-08 14:13:52 +0530

    secound part completed - Ui designe Part

---
+++ frontend/src/pages/signup/SignUp.tsx
@@ -0,0 +1,72 @@
+import GenderCheckbox from "./GenerateCheckbox";
+
+const SignUp = () => {
+  return (
+    <div className="flex flex-col items-center justify-center min-w-96 mx-auto">
+      <div className="w-full p-6 rounded-lg shadow-md bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0 ">
+        <h1 className="text-3xl font-semibold text-center">
+          Sign Up <span className="text-blue-500">WinChat</span>
+        </h1>
+
+        <form action="">
+          <div>
+            <label className="label p-2">
+              <span className="text-base label-text">Full Name</span>
+            </label>
+            <input
+              type="text"
+              placeholder="Enter your fullname"
+              className="input w-full input-bordered h-10"
+            />
+          </div>
+          <div>
+            <label className="label p-2">
+              <span className="text-base label-text">Username</span>
+            </label>
+            <input
+              type="text"
+              placeholder="Create a unique username"
+              className="input w-full input-bordered h-10"
+            />
+          </div>
+          <div>
+            <label className="label p-2">
+              <span className="text-base label-text">Email</span>
+            </label>
+            <input
+              type="Email"
+              placeholder="Enter your Email"
+              className="input w-full input-bordered h-10"
+            />
+          </div>
+          <div>
+            <label className="label p-2">
+              <span className="text-base label-text">Password</span>
+            </label>
+            <input
+              type="password"
+              placeholder="Enter password"
+              className="input w-full input-bordered h-10"
+            />
+          </div>
+
+          {/* GENDER CHECKBOX GOES HERE */}
+          <GenderCheckbox />
+
+          <a
+            href="#"
+            className="text-sm hover:underline hover:text-blue-600 mt-2"
+          >
+            {"Already"} have an account?
+          </a>
+
+          <div>
+            <button className="btn btn-block btn-sm mt-2">Sign Up</button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default SignUp;
